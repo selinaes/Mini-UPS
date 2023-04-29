241180b76b72a41f66074c103192a393e196ac9e
yl906 <yikai.liu@duke.edu>
2023-04-29 18:15:56 -0400

    Add more UI

--- ups/ups_website/urls.py
+++ ups/ups_website/urls.py
@@ -27,11 +27,11 @@ from . import views
 app_name = 'ups_website'
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.request_tracking, name="request_shipment"),
     path('accounts/login/', views.login, name='login'),
     path('accounts/logout/', views.logout, name='logout'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('request_shipment/', views.request_tracking, name='request_shipment'),
+    path('request_shipment/', views.index, name='index'),
     path('find_shipments/', views.find_all_shipments, name='find_shipments'),
     path('find_packages_detail/<int:shipment_id>/', views.find_packages_detail, name='find_packages_detail'),
     path('change_address/<int:shipment_id>/', views.change_address, name='change_address'),
